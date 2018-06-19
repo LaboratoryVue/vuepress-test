a3735e36355d884e87397f5c30c29eeba60189b6
Moe Green <gearmobile@gmail.com>
2018-06-19 20:43:07 +0300

    fill vuepress folder

---
+++ docs/.vuepress/components/counter.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="counter">
+    <h1 class="counter__title">{{ number }}</h1>
+    <button class="counter__button" type="button" @click="increment">Increment</button>
+    <button class="counter__button" type="button" @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        number: 0,
+      }
+    },
+    methods:{
+      increment(){
+        if(this.number >= 0){
+          this.number++
+        }
+      },
+      decrement(){
+        if(this.number > 0){
+          this.number --
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .counter {
+    display: inline-block;
+    margin-left: 30%;
+  }
+
+  .counter__title {
+    text-align: center;
+  }
+
+  .counter__button {
+    display: inline-block;
+    padding: 20px;
+    margin: 10px;
+    font-weight: 700;
+    border-radius: 5px;
+    box-shadow: 0px 0px 5px 0px rgb(11, 11, 114);
+  }
+
+
+</style>
---
+++ docs/.vuepress/components/my-header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>This Header is actually a Vue Template</h1>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
