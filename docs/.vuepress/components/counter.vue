7236a6b157f2adc41422dad3a81c94fb3efde023
Moe Green <gearmobile@gmail.com>
2018-06-19 20:54:22 +0300

    fix all

--- docs/.vuepress/components/counter.vue
+++ docs/.vuepress/components/counter.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="counter">
     <h1 class="counter__title">{{ number }}</h1>
-    <button class="counter__button" type="button" @click="increment">Increment</button>
-    <button class="counter__button" type="button" @click="decrement">Decrement</button>
+    <button class="counter__button" type="button" @click="increment()">increment</button>
+    <button class="counter__button" type="button" @click="decrement()">decrement</button>
   </div>
 </template>
 
 <script>
   export default {
-    data(){
+    data() {
       return{
         number: 0,
       }
     },
     methods:{
-      increment(){
-        if(this.number >= 0){
-          this.number++
+      increment() {
+        if (this.number >= 0) {
+          this.number += 1
         }
       },
-      decrement(){
-        if(this.number > 0){
-          this.number --
+      decrement() {
+        if (this.number > 0) {
+          this.number -= 1
         }
       }
     }
@@ -46,7 +46,7 @@
     font-weight: 700;
     border-radius: 5px;
     box-shadow: 0px 0px 5px 0px rgb(11, 11, 114);
+    text-transform: capitalize;
   }
-
 
 </style>
